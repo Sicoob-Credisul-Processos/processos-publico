aa5529e5490c0ec55cba387a934923b5cbf7dbf1
JULIANO MELLO <juliano.mello@sicoobcredisul.com.br>
2024-04-05 10:21:25 -0400

    criando funcoes genericas para campo opcoes

--- index.js
+++ index.js
@@ -1,7 +1,142 @@
 console.info("Iniciando o script Sicoob Zeev");
 const SicoobZeev = {
+    geral: {
+        obterOpcoesJsonHierarquico: (objetoJson, ...args) => {
+            // Estrutura do JSON hierárquico
+            const dados = objetoJson;
+
+            // Verifica se nenhum argumento foi passado
+            if (args.length === 0) {
+                return Object.keys(dados);
+            }
+
+            // Verifica se o primeiro argumento é um nó válido no JSON
+            if (dados[args[0]]) {
+                // Se tiver apenas um argumento, retorna as chaves do nó correspondente
+                if (args.length === 1) {
+                    return Object.keys(dados[args[0]]);
+                }
+
+                // Se tiver mais de um argumento, verifica se os nós existem e retorna suas chaves
+                let currentNode = dados[args[0]];
+                for (let i = 1; i < args.length; i++) {
+                    if (!currentNode[args[i]]) {
+                        return [];
+                    }
+                    currentNode = currentNode[args[i]];
+                }
+                return Object.keys(currentNode);
+            }
+
+            // Caso nenhum caso corresponda, retorna um array vazio
+            return [];
+        }
+    },
+    ferramentasHTML: {
+        mostrarCampos: (idCampoSelecao, valoresVisiveis) => {
+            //exemplo (inpCidades, ["Valor A", "Valor B"])
+            var campoSelecao = document.getElementById(idCampoSelecao);
+            var opcoes = campoSelecao.querySelectorAll("option");
+
+            for (const opcao of opcoes) {
+                if (valoresVisiveis.includes(opcao.value)) {
+                    opcao.style.display = ""; // Remove o estilo display: none
+                } else {
+                    opcao.style.display = "none";
+                }
+            }
+        },
+        // Função para mostrar apenas o valor especificado no campo de seleção
+        mostrarApenasUmValorNoCampoSelecao: (id, valor) => {
+            let selectElement = document.getElementById(id);
+            let options = selectElement.getElementsByTagName("option");
+
+            for (let i = 0; i < options.length; i++) {
+                if (options[i].value == valor) {
+                    options[i].style.display = "block"; // Exibe o valor se for igual ao especificado
+                } else {
+                    options[i].style.display = "none"; // Oculta os demais valores
+                }
+            }
+        },
+
+        // Função para mostrar todos os valores no campo de seleção
+        mostrarTodosValoresNoCampoSelecao: (id) => {
+            let selectElement = document.getElementById(id);
+            let options = selectElement.querySelectorAll('option');
+
+            for (let i = 0; i < options.length; i++) {
+                options[i].style.display = "inline"; // Exibe todos os valores
+            }
+        },
+
+        // Função para ocultar todos os valores no campo de seleção e limpar a seleção
+        ocultarTodosValoresNoCampoSelecao: (id) => {
+            let selectElement = document.getElementById(id);
+            let options = selectElement.querySelectorAll('option');
+
+            for (let i = 0; i < options.length; i++) {
+                options[i].style.display = "none"; // Oculta todos os valores
+            }
+
+            selectElement.value = ""; // Limpa a seleção
+        },
+
+        // Função para exibir apenas os valores presentes no array no campo de seleção
+        exibirValoresDoArrayNoCampoSelecao: (id, arrayDeValores) => {
+            let selectElement = document.getElementById(id);
+
+            for (let i = 0; i < selectElement.options.length; i++) {
+                selectElement.options[i].style.display = "none"; // Oculta todos os valores
+            }
+
+            for (valor of arrayDeValores) {
+                selectElement.querySelector(`option[value="${valor}"]`).style.display = "inline"; // Exibe os valores presentes no array
+            }
+        },
+
+        // Função para ocultar os valores que contêm no array no campo de seleção
+        ocultarValoresQueContemNoArrayNoCampoSelecao: (id, arrayDeValores) => {
+            let selectElement = document.getElementById(id);
+
+            for (valor of arrayDeValores) {
+                selectElement.querySelector(`option[value="${valor}"]`).style.display = "none"; // Oculta os valores presentes no array
+            }
+        },
+
+        // Função para excluir todos os valores no campo de seleção
+        excluirTodosValoresNoCampoSelecao: (id) => {
+            let selectElement = document.getElementById(id);
+            let options = selectElement.querySelectorAll('option');
+
+            for (let i = 0; i < options.length; i++) {
+                selectElement.remove(0); // Remove todos os valores
+            }
+        },
+
+        // Função para excluir os valores que contêm no array no campo de seleção
+        excluirValoresQueContemNoArrayNoCampoSelecao: (id, arrayDeValores) => {
+            let selectElement = document.getElementById(id);
+            for (valor of arrayDeValores) {
+                selectElement.querySelector(`option[value="${valor}"]`).remove(0); // Remove os valores presentes no array
+            }
+        },
+
+        // Função para mostrar apenas os valores que contêm o texto especificado no campo de seleção
+        mostrarApenasValoresQueContemUmTextoNoCampoDeSelecao: (id, texto) => {
+            let selectElement = document.getElementById(id);
+            let options = selectElement.querySelectorAll('option');
+
+            for (let i = 0; i < options.length; i++) {
+                if (!options[i].text.includes(texto)) {
+                    options[i].style.display = "none"; // Oculta os valores que não contêm o texto especificado
+                }
+            }
+        }
+
+    },
     validadores: {
-        validarCPFCNPJ: function validarCPFCNPJ(documento) {
+        validarCPFCNPJ: (documento) => {
             documento = documento.replace(/[^\d]/g, '')
 
             if (documento.length === 11) {
@@ -17,7 +152,7 @@ const SicoobZeev = {
             return { tipo: 'Inválido', valido: false }
         },
 
-        validarCPF: function validarCPF(cpf) {
+        validarCPF: (cpf) => {
             // Validação do CPF
             cpf = cpf.replace(/[^\d]/g, '')
 
@@ -42,7 +177,7 @@ const SicoobZeev = {
             return parseInt(cpf.charAt(9)) === digitoVerificador1 && parseInt(cpf.charAt(10)) === digitoVerificador2
         },
 
-        validarCNPJ: function validarCNPJ(cnpj) {
+        validarCNPJ: (cnpj) => {
             // Validação do CNPJ
             cnpj = cnpj.replace(/[^\d]/g, '')
 
