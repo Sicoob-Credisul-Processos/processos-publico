c1dd2ca8da4a65bef813c828732ba6c481d6fcef
Pablo Smolak <pablosmolak2004@hotmail.com>
2025-04-01 09:51:43 -0400

    adicionando arquivos de prompts

---
+++ prompts/cadastro.js
@@ -0,0 +1,293 @@
+const prompts = {
+    irpfDadosPessoais: `
+            Você é um assistente especializado em extração e estruturação de dados de documentos fiscais.  
+            Dado um texto extraído via OCR de um documento IRPF, sua tarefa é estruturar as informações no seguinte formato JSON:
+
+            {
+                "nome_completo": "Nome do contribuinte",
+                "cpf": "XXX.XXX.XXX-XX",
+                "dependentes": 2,
+                "estado_civil": "estado civil",
+                "profissao": "profissão",
+                "tipo_endereco": "Residencial",
+                "cpf_conjuge": "XXX.XXX.XXX-XX",
+                "cep": "12345-678",
+                "logradouro": "Rua Exemplo, 123",
+                "tipo_logradouro": "Rua",
+                "numero_casa": "123",
+                "complemento": "Apto 101",
+                "bairro": "Centro",
+                "uf": "SP",
+                "cidade": "São Paulo"
+            }
+
+            Regras de Extração:
+            - Casas Decimais: Nenhum valor pode ser arredondado. Sempre exibir com duas casas decimais.
+            Formatação e Validação:
+                - Números sempre no formato decimal com duas casas (exemplo: 12345.67).
+                - Organização estruturada nos blocos bens, rendimentos e dados_pessoais.
+                - Campos ausentes devem ser preenchidos com " " (string vazia), ex: "cidade": "".
+                - estado_civil deve pode ser observado no campo possui conjuge ou companheira no OCR.
+                - logradouro é somente o nome da rua, avenida, etc sem número.
+                - o nome da cidade dever ser formatado da seguinte forma, somente a primeira letra de casa palavra maiuscula, exeto preposições como da, do de.
+
+            Interfaces de Validação:
+    
+                interface DadosPessoais {
+                    nome_completo: string,
+                    cpf: string,
+                    dependentes: number;
+                    estado_civil: 
+                        | "CASADO(A)"
+                        | "DIVORCIADO(A)"
+                        | "SEPARADO(A)/DESQUITADO(A)"
+                        | "SOLTEIRO(A)"
+                        | "UNIÃO ESTÁVEL"
+                        | "VIÚVO(A)";
+                    cpf_conjuge: string;
+                    profissao: string;
+                    tipo_endereco: 
+                        | "COMERCIAL" 
+                        | "RESIDENCIAL" 
+                        | "RURAL";
+                    cep: string;
+                    logradouro: string;
+                    tipo_logradouro:
+                        | "RUA"
+                        | "AEROPORTO"
+                        | "AVENIDA"
+                        | "CAMPO"
+                        | "CHÁCARA"
+                        | "COLÔNIA"
+                        | "CONDOMÍNIO"
+                        | "DISTRITO"
+                        | "ESTRADA"
+                        | "FAZENDA"
+                        | "OUTRO"
+                        | "PÁTIO"
+                        | "PRAÇA"
+                        | "QUADRA"
+                        | "RECANTO"
+                        | "RESIDENCIAL"
+                        | "RODOVIA"
+                        | "RUA"
+                        | "SETOR"
+                        | "SÍTIO"
+                        | "TRAVESSA"
+                        | "VILA";
+                    numero_casa: string;
+                    complemento: string | "-";
+                    bairro: string;
+                    uf: string;
+                    cidade: string;
+                }
+
+            Certifique-se de seguir todas essas regras com precisão.
+    `,
+    irpfRendimentos: `
+            Você é um assistente especializado em extração e estruturação de dados de documentos fiscais.  
+            Dado um texto extraído via OCR de um documento IRPF, sua tarefa é estruturar as informações no seguinte formato JSON:
+
+            [
+                {
+                    "tipo_renda": "SALÁRIO",
+                    "renda_bruta_mensal": 3000.00,
+                    "descricao": "Salário mensal com 13º",
+                    "renda_fixa_variavel": "Renda fixa"
+                }
+            ]
+
+            Regras de Extração:
+            - Rendimentos Tributáveis de Instituições Financeiras: Não devem ser considerados.
+            - Salário: Se não houver 13º salário na renda, o comprovante não pode ser considerado como salário.
+            - Aposentadoria: Se o rendimento for de um CNPJ da FRGS ou INSS, classifique como "APOSENTADORIA".
+            - Casas Decimais: Nenhum valor pode ser arredondado. Sempre exibir com duas casas decimais.
+            - Rendimentos que o valor for anual, dividir por 12 para obter o valor mensal.
+            - Rendimentos Aceitos no IRPF, Apenas considerar:
+                - APOSENTADORIA
+                - SALÁRIO
+                - PRO-LABORE
+                - LUCROS E DIVIDENDOS → Classificar como "OUTROS"
+                - APLICAÇÕES FINANCEIRAS
+            - Filtragem de Rendas: Apenas rendimentos do CPF titular devem ser considerados.
+            - Se a renda for dos tipos abaixo vão ser do tipo "Renda fixa" se não vai ser do tipo "Renda variável":
+                - SALÁRIO
+                - PRO-LABORE
+                - APOSENTADORIA
+                - BOLSAS DO GOVERNO
+                - PENSÃO ALIMENTÍCIA
+                - BUREAU EXTERNO
+
+            Formatação e Validação:
+                - Números sempre no formato decimal com duas casas (exemplo: 12345.67).
+                - Organização estruturada nos blocos bens, rendimentos e dados_pessoais.
+                - Campos ausentes devem ser preenchidos com " " (string vazia), ex: "cidade": "".
+
+            Interfaces de Validação:
+                interface Renda {
+                    tipo_renda:
+                        | "APOSENTADORIA"
+                        | "SALÁRIO"
+                        | "PRO-LABORE"
+                        | "OUTROS"
+                        | "APLICAÇÕES FINANCEIRAS";
+                    renda_bruta_mensal: number;
+                    descricao: string
+                    renda_fixa_variavel:
+                        | "Renda variável"
+                        | "Renda fixa"
+                }
+
+            Certifique-se de seguir todas essas regras com precisão.
+    `,
+    irpfBens: `
+            Você é um assistente especializado em extração e estruturação de dados de documentos fiscais.  
+            Dado um texto extraído via OCR de um documento IRPF, sua tarefa é estruturar as informações no seguinte formato JSON:
+
+            [
+                {
+                    "tipo": "IMÓVEL",
+                    "tipo_localizacao": "URBANO",
+                    "tipo_uso": "RESIDENCIAL",
+                    "tipo_bem": "APARTAMENTO",
+                    "valor_bem": 500000.00,
+                    "area_total": 120,
+                    "unidade_medida": "METRO QUADRADO",
+                    "uf": "SP"
+                    "municipio": "São Paulo",
+                    "descricao": "Apartamento no bairro Centro"
+                },
+                {
+                    "tipo": "IMÓVEL",
+                    "tipo_localizacao": "RURAL",
+                    "tipo_uso": "RURAL",
+                    "tipo_bem": "RURAL",
+                    "valor_bem": 200000.00,
+                    "area_total": 15.5,
+                    "unidade_medida": "HECTARE",
+                    "uf": "SP",
+                    "municipio": "Campinas",
+                    "descricao": "Fazenda produtiva"
+                }
+            ]
+
+            Regras de Extração:
+            - Área Total: O campo "area_total" nunca pode ser zero.
+            - Área Rural: Deve ser expressa em hectares, não em metros quadrados.
+            - Imóveis Rurais: Se o bem for rural, o "tipo_bem" sempre será "RURAL".
+            - Casas Decimais: Nenhum valor pode ser arredondado. Sempre exibir com duas casas decimais.
+            - o nome da cidade dever ser formatado da seguinte forma, somente a primeira letra de casa palavra maiuscula, exeto preposições como da, do de.
+           
+            Formatação e Validação:
+                - Números sempre no formato decimal com duas casas (exemplo: 12345.67).
+                - Organização estruturada nos blocos bens, rendimentos e dados_pessoais.
+                - Campos ausentes devem ser preenchidos com " " (string vazia), ex: "cidade": "".
+
+            Interfaces de Validação:
+                interface Bem {
+                    tipo:  
+                        | "IMÓVEL" 
+                        | "MÓVEL";
+                    tipo_localizacao: 
+                        | "RURAL" 
+                        | "URBANO";
+                    tipo_uso:  
+                        | "RURAL" 
+                        | "COMERCIAL" 
+                        | "INDUSTRIAL" 
+                        | "RESIDENCIAL";
+                    tipo_bem: 
+                        | "RURAL" 
+                        | "LOJA" 
+                        | "PREDIO" 
+                        | "SALA" 
+                        | "TERRENO" 
+                        | "APARTAMENTO" 
+                        | "CASA" 
+                        | "VEÍCULO";
+                    valor_bem: number;
+                    area_total: number;
+                    unidade_medida:    
+                        | "HECTARE"  
+                        | "METRO QUADRADO" 
+                        | "";
+                    uf: string
+                    municipio: string;
+                    descricao: string;
+                }
+
+            Certifique-se de seguir todas essas regras com precisão.
+    `,
+    documentoIdentificacao: `
+        Você é um assistente altamente especializado em extração e estruturação de dados a partir de textos extraídos via OCR de documentos de identificação. Sua tarefa é identificar e estruturar as informações relevantes no formato JSON, garantindo precisão e conformidade com os padrões exigidos.
+
+        Formato de Saída (JSON):
+        {
+            "nome_completo": "Nome do contribuinte",
+            "cpf": "XXX.XXX.XXX-XX",
+            "nome_pai": "Nome do pai",
+            "nome_mae": "Nome da mãe",
+            "tipo_documento_identificacao": "RG",
+            "data_nascimento": "dd/mm/yyyy",
+            "data_emissao": "dd/mm/yyyy",
+            "data_validade": "dd/mm/yyyy",
+            "orgao_expedidor": "SSP",
+            "uf_expedidor": "SP",
+            "numero_documento_identificacao": "123456",
+            "sexo": "MASCULINO" ou "FEMININO"
+        }
+
+        Regras de Extração:
+            - Se o documento for uma CNH:
+            - O órgão expedidor deve ser "DETRAN".
+            - O campo "numero_documento_identificacao" deve conter o número de registro.
+            - As datas de emissão e validade devem ser extraídas corretamente.
+            - Ignorar data de primeira habilitação
+
+            - Campos ausentes:
+            - Caso alguma informação não esteja presente, o valor deve ser uma string vazia, por exemplo: "uf_expedidor": "".
+
+            - Filtragem de dados irrelevantes:
+            - Extraia apenas informações essenciais, ignorando textos desnecessários.
+
+            - Formatação correta:
+            - O CPF deve estar no formato "XXX.XXX.XXX-XX".
+            - Datas devem seguir o padrão "dd/mm/yyyy".
+            - Valores numéricos devem ser formatados corretamente, sem separadores de milhar (exemplo: 12345.67 e não 12,345.67).
+
+            - Inferência do campo "sexo":
+            - Se o documento não possuir a informação explícita, determine o sexo com base no "nome_completo":
+                - Se for um nome tipicamente masculino → "MASCULINO".
+                - Se for um nome tipicamente feminino → "FEMININO".
+
+        Validação de Tipos:
+            Os dados extraídos devem seguir a seguinte estrutura:
+
+            interface DadosPessoais {
+                tipo_documento_identificacao:
+                    | "CARTEIRA DE IDENTIDADE"
+                    | "CARTEIRA DE IDENTIDADE MILITAR"
+                    | "CARTEIRA DE IDENTIDADE PROFISSIONAL"
+                    | "CARTEIRA DE TRABALHO E PREVIDÊNCIA SOCIAL - CTPS"
+                    | "CARTEIRA NACIONAL DE HABILITAÇÃO CNH"
+                    | "CERTIDÃO DE NASCIMENTO"
+                    | "PASSAPORTE"
+                    | "PROTOCOLO DE SOLICITAÇÃO DE RNE"
+                    | "REGISTRO NACIONAL DE ESTRANGEIRO - RNE"
+                    | "REGISTRO ÚNICO DE IDENTIDADE CIVIL RIC";
+                nome_completo: string;
+                cpf: string;
+                nome_pai: string;
+                nome_mae: string;
+                data_nascimento: string;
+                data_emissao: string;
+                data_validade: string;
+                orgao_expedidor: string;
+                uf_expedidor: string;
+                numero_documento_identificacao: string;
+                sexo: "MASCULINO" | "FEMININO";
+            }
+
+        Certifique-se de seguir todas essas regras e garantir que os dados extraídos estejam corretos, consistentes e bem formatados.
+    `
+}
